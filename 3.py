077366db778cccfd8a38848cfd67542c819443a4
Kim-jinju <merriment9030@gmail.com>
2016-10-18 20:24:40 +0900

    yayayayayayayayaya

--- 3.py
+++ 3.py
@@ -1,8 +1,10 @@
 a = [4, 5, 6, 2, 1]
+
 for i in range(len(a)):
     for j in range(i+1, len(a)):
         if a[i] > a[j]:
             t = a[i]
             a[i] = a[j]
             a[j] = t
+
 print(a)
